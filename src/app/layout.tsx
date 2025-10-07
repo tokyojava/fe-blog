47dab2cfd2c39c8a9649d77a31309e2c7f903dd5
sakop <454519934@qq.com>
2025-10-07 14:07:57 +0800

    add register form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full py-2 px-8`}
       >
         {children}
       </body>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Field, FieldDescription, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
 
 export default function SignupPage() {
     return (
-        <div className="bg-muted mt-4 max-w-2/3 flex justify-center m-auto align-center w-full">
-            <Card className="w-full">
+        <div className={"w-full h-full relative"}>
+            <Card className="w-1/2 absolute top-1/6 left-1/2 -translate-x-1/2">
                 <CardHeader>
                     <CardTitle>
                         Create your account
@@ -15,9 +18,40 @@ export default function SignupPage() {
 
                 </CardHeader>
                 <CardContent>
+                    <form>
+                        <FieldGroup>
+                            <Field>
+                                <FieldLabel htmlFor="username">Full Name</FieldLabel>
+                                <Input id="username" type="input" placeholder="Jane Doe" />
+                            </Field>
+                            <Field>
+                                <FieldLabel htmlFor="email">Email</FieldLabel>
+                                <Input id="email" type="input" placeholder="m@example.com" />
+                            </Field>
+                            <Field className="grid grid-cols-2 gap-4">
+                                <Field>
+                                    <FieldLabel htmlFor="pw1">Password</FieldLabel>
+                                    <Input id="pw1" type="password" />
+                                </Field>
+                                <Field>
+                                    <FieldLabel htmlFor="pw2">Confirm Password</FieldLabel>
+                                    <Input id="pw2" type="password" />
+                                </Field>
+                                <FieldDescription>
+                                    Must be at least 8 characters long.
+                                </FieldDescription>
+                            </Field>
+                            <Field>
+                                <Button>Create Account</Button>
+                                <FieldDescription className="text-center">
+                                    Already have an account? <a href="#">Sign in</a>
+                                </FieldDescription>
+                            </Field>
+                        </FieldGroup>
 
+                    </form>
                 </CardContent>
-            </Card>
-        </div>
+            </Card >
+        </div >
     )
 }
